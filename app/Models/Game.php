3cbb23afb9d63e5b71c205a99469bd2d850e70b5
Georgia Harrison <georgiaharrison1994@gmail.com>
2021-03-22 15:13:29 +0000

    Initial commit

---
+++ app/Models/Game.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ["title", "min_players", "max_players", "difficulty", "time"];
+    public $timestamps = false;
+}
---
+++ gamesnotes.md
@@ -0,0 +1,7 @@
+$game = new Game();
+$game->title = "Dungeons and Dragons";
+$game->min_players = 2;
+$game->max_players = 10;
+$game->difficulty = 3;
+$game->time = 300;
+$game->save();
