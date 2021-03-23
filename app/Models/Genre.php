0ee93fad71d1b0a1bbcdc0d0243979d8d7702ce0
Georgia Harrison <georgiaharrison1994@gmail.com>
2021-03-23 11:45:50 +0000

    Fixed genres bug in show method of Game Controller

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -7,15 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Genre extends Model
 {
-<<<<<<< HEAD
-    public $timestamps = false;
-    
-=======
 
     public $timestamps = false;
     protected $fillable = ["genre"];
 
->>>>>>> 7ad3d7ca20ff35d57638d0150d7c1dde2ec53f8e
     public function games()
     {
         return $this->belongsToMany(Game::class);
@@ -25,10 +20,6 @@ class Genre extends Model
     {
         return collect($strings)->map(fn($str) => trim($str))
                                 ->unique()
-<<<<<<< HEAD
-                                ->map(fn($str) => Game::firstOrCreate(["name" => $str]));
-=======
                                 ->map(fn($str) => Genre::firstOrCreate(["genre" => $str]));
->>>>>>> 7ad3d7ca20ff35d57638d0150d7c1dde2ec53f8e
     }
 }
